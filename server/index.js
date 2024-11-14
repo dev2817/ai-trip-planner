480adb2fb520971d0c1411902da20a362098862c
devsh17 <dev.shah@thegatewaycorp.co.in>
2024-11-14 19:00:45 +0530

    adding port

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors())
 
 app.use('/api', router)
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`server listening to ${port}`);
